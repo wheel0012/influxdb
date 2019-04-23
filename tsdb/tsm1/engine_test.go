fffb8f7b2a2397fdfbeec55c54d27e3821a2bbda
Jeff Wendling <leterip@gmail.com>
2019-04-22 22:43:42 -0600

    tsm1: implement reading and writing predicates in tombstone files

--- tsdb/tsm1/engine_test.go
+++ tsdb/tsm1/engine_test.go
@@ -477,15 +477,15 @@ func (e *Engine) MustWritePointsString(org, bucket influxdb.ID, buf string) {
 	}
 }
 
-// MustDeleteBucketRange calls DeleteBucketRange using the org and bucket for
-// the name. Panic on error.
+// MustDeleteBucketRange calls DeletePrefixRange using the org and bucket for
+// the prefix. Panic on error.
 func (e *Engine) MustDeleteBucketRange(orgID, bucketID influxdb.ID, min, max int64) {
 	// TODO(edd): we need to clean up how we're encoding the prefix so that we
 	// don't have to remember to get it right everywhere we need to touch TSM data.
 	encoded := tsdb.EncodeName(orgID, bucketID)
 	name := models.EscapeMeasurement(encoded[:])
 
-	err := e.DeleteBucketRange(name, min, max)
+	err := e.DeletePrefixRange(name, min, max, nil)
 	if err != nil {
 		panic(err)
 	}
